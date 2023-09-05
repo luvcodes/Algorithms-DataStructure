17e75978b791710fd4a1f36884528211bdd55bdf
Runze Yang <ryan0028@student.monash.edu>
2023-09-05 21:03:29 +1000

    update on the content

--- 数组与链表/链表/删除节点/deleteNode.java
+++ 数组与链表/链表/删除节点/deleteNode.java
@@ -5,7 +5,7 @@ package 删除节点;
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode (int x) {val = x; }
  * }
  */
 class Solution {
@@ -13,6 +13,7 @@ class Solution {
         /**
         常用的删除链表汇总的节点的常见的方法是定位到待删除节点的上一个节点，
         修改上一个节点的next指针，使其指向待删除节点的下一个节点。
+
         但是在这个题，传入的node为要被删除的节点，无法定位到该节点的上一个节点。
         可以通过修改node的next指针的指向，删除node的下一个节点。但是题目要求删除node，
         为了达到删除node的效果，只要在删除节点之前，将node的节点值
